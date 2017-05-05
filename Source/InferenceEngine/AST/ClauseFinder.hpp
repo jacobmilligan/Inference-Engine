df9cbdbb7660e2e8b124fa41f027ad1ad185cc5d
Jacob Milligan <jacobpmilligan@gmail.com>
2017-05-05 16:11:10 +1000

    Feature: Add clause finder and associated tests

---
+++ Source/InferenceEngine/AST/ClauseFinder.hpp
@@ -0,0 +1,48 @@
+//
+//  ClauseFinder.hpp
+//  iengine
+//
+//  --------------------------------------------------------------
+//
+//  Created by
+//  Jacob Milligan on 5/05/2017
+//  Copyright (c) 2016 Jacob Milligan. All rights reserved.
+//
+
+#pragma once
+
+#include "InferenceEngine/AST/ASTVisitor.hpp"
+
+namespace ie {
+
+/// @brief ClauseFinder finds complex sentences and atomics and places
+/// them into separate containers for iteration
+class ClauseFinder : public ASTVisitor {
+public:
+    /// @brief Visits a sentence - does nothing
+    /// @param sentence Sentence to visit
+    void visit(const Sentence& sentence) override;
+
+    /// @brief Visits an atomic and if it's not already discovered, places it
+    /// into the symbol container
+    /// @param atom The atomic to visit
+    void visit(const AtomicSentence& atom) override;
+
+    /// @brief Visits a complex sentence and adds it to the rules container
+    /// @param complex Complex sentence to visit
+    void visit(const ComplexSentence& complex) override;
+
+    /// @brief Gets the container of rules
+    /// @return Rules container
+    const std::vector<const ComplexSentence*>& rules() const;
+
+    /// @brief Gets all symbols found by the clause finder
+    /// @return The symbols container
+    const std::vector<std::string>& symbols() const;
+private:
+    std::vector<const ComplexSentence*> rules_;
+    std::vector<std::string> atomics_;
+};
+
+
+}
---
+++ Tests/VisitorTests.cpp
@@ -0,0 +1,70 @@
+//
+//  VisitorTests.cpp
+//  iengine
+//
+//  --------------------------------------------------------------
+//
+//  Created by
+//  Jacob Milligan on 5/05/2017
+//  Copyright (c) 2016 Jacob Milligan. All rights reserved.
+//
+
+#include "CoutUtils.hpp"
+
+#include <InferenceEngine/Parsing/Parser.hpp>
+#include <InferenceEngine/AST/ClauseFinder.hpp>
+
+#define CATCH_CONFIG_MAIN
+#include <catch/catch.hpp>
+#include <InferenceEngine/AST/Private/ASTPrinter.hpp>
+
+TEST_CASE("ClauseFinder separates complex and atoms", "[rules]")
+{
+    CoutUtils cout_utils;
+
+    std::string rules = "p=>q; p; p; q; p&q=>!b; b; !b&!q=>(p|q)&q";
+    ie::Parser parser;
+    parser.parse(rules);
+
+    ie::ClauseFinder rule_finder;
+    ie::ASTPrinter printer;
+
+    for ( auto& n : parser.ast() ) {
+        n->accept(rule_finder);
+    }
+
+    SECTION("ClauseFinder finds only complex")
+    {
+        std::vector<std::string> expected = {
+            "(p=>q)",
+            "((p&q)=>(!b))",
+            "(((!b)&(!q))=>((p|q)&q))"
+        };
+
+        int index = 0;
+        for ( auto& c : rule_finder.rules() ) {
+            cout_utils.redirect_cout();
+            c->accept(printer);
+            REQUIRE(cout_utils.get_cout() == expected[index]);
+            cout_utils.reset_cout();
+            ++index;
+        }
+    }
+
+    SECTION("ClauseFinder finds only symbols")
+    {
+        std::vector<std::string> expected = {
+            "p",
+            "q",
+            "b"
+        };
+
+        int index = 0;
+        for ( auto& c : rule_finder.symbols() ) {
+            REQUIRE(c == expected[index]);
+            ++index;
+        }
+    }
+
+
+}
